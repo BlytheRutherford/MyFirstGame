b85a8adda2daf75e0a4673a3c631e8475138dac5
BlytheRutherford <blythe.rutherford@proton.me>
2025-12-04 15:51:38 -0600

    Add physics system with Character Controller
    - Added Character Controller component for proper collision detection
    - Implemented gravity system to keep player grounded
    - Movement now respects terrain slopes and elevation
    - Fixed issue where cube would float through terrain
    - Refactored movement code to use controller.Move() instead of transform.Translate()

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,12 +4,31 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float rotateSpeed = 100f;
+    private float gravity = -9.81f;
+    private float verticalVelocity = 0f;
+    private CharacterController controller;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
     void Update()
     {
         float rotate = Input.GetAxis("Horizontal");
         float move = Input.GetAxis("Vertical");
-        transform.Rotate(0, rotate * rotateSpeed * Time.deltaTime, 0, Space.World);
-        Vector3 movement = transform.forward * move * moveSpeed * Time.deltaTime;
-        transform.Translate(movement, Space.World);
+        transform.Rotate(0, rotate * rotateSpeed * Time.deltaTime, 0);
+        Vector3 moveDirection = transform.forward * move * moveSpeed;
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -2f;
+        }
+        else
+        {
+            verticalVelocity += gravity * Time.deltaTime;
+        }
+
+        moveDirection.y = verticalVelocity;
+
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
